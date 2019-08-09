41e8d27708901c916c2a358911a11e9d28aacaca
root <andyb@datadevil.co.uk>
2019-08-09 10:35:00 +0100

    Small fixes and comments

--- eosjs_server/app.js
+++ eosjs_server/app.js
@@ -1,3 +1,6 @@
+// basic http server which uses eosjs for signing and pushing transactions to API node
+// only called within docker subnet - DO NOT OPEN PORT 3000 IN PRODUCTION!
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -8,7 +11,7 @@ const fetch = require('node-fetch');
 const { TextEncoder, TextDecoder } = require('util');
 const key = process.env.SUBMISSION_PRIVATE_KEY;
 const signatureProvider = new JsSignatureProvider([key]);
-const rpc = new JsonRpc(process.env.EOSIO_API_NODE_1, { fetch });
+const rpc = new JsonRpc(process.env.EOSIO_API_NODE, { fetch });
 const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
 
 app.use(express.json());
@@ -18,9 +21,8 @@ app.get('/', (req, res) => {
 })
 
 app.post('/push_transaction', async (req, res) => {
-  var tx = req.body;
   try {
-    const result = await api.transact(tx, {'blocksBehind': 3, 'expireSeconds': 30});
+    const result = await api.transact(req.body, {'blocksBehind': 3, 'expireSeconds': 30});
     res.send(result)
   } catch (e) {
     res.send({"error": "" + e})
